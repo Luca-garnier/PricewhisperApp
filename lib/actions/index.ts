c1895630511b155443f97888b72b525cc189bf8b
Luca-garnier <Luca.garnier@gmail.com>
2023-10-09 15:25:49 -0400

    bright data configuration setup

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,10 +1,13 @@
 "use server"
 
+import { scrapeAmazonProduct } from "../scraper";
+
 //Scrape product and save it in our DB 
-export async function scrapeAndStorePRoduct(productUrl: string) {
+export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
 
     try {
+        const scrapedProduct = await scrapeAmazonProduct(productUrl);
         
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`);
